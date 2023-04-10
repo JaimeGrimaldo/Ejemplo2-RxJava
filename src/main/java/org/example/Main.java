6558a347b781ea795e333db69feadcbeb196576b
JaimeGrimaldo <DankerGamer-@hotmail.com>
2023-04-10 09:51:46 -0600

    Subiendo proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class Main {
+    public static void main(String[] args) {
+        Observable<String> observable = Observable.just("Hello");
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                System.out.println("onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+        observable.subscribe(observer);
+    }
+}
+
+
+    //Este ejemplo crea un Observable que emite una cadena “Hello” y un Observer que recibe la cadena y
+// la imprime en la consola. Luego se suscribe el Observer al Observable.
